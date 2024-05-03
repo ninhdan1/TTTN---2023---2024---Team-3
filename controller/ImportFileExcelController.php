09fcd6d84b54fa5b0a5340f083e52ccda12bf114
Vnquang8 <trantrongphat16072002@gmail.com>
2024-05-04 00:26:13 +0700

    Module Thống kê khối lượng giảng dạy (21/04)

--- controller/ImportFileExcelController.php
+++ controller/ImportFileExcelController.php
@@ -1,31 +1,57 @@
 <?php
 
-require_once __DIR__. '/../model/ImportFileExcel.php';
-require_once __DIR__. '/../model/SQLQueries.php';
-require_once __DIR__. '/../DB/DBConnect.php';
+require_once __DIR__ . '/../helper/ConfigHelper.php';
 
-class ImportFileExcelController{
+require_once MODEL_PATH . 'ImportFileExcel.php';
+require_once MODEL_PATH . 'SQLQueries.php';
+require_once DB_PATH . 'DBConnect.php';
+
+class ImportFileExcelController
+{
     private $model;
     private $conn;
     private $sqlQueries;
 
-    public function __construct() {
-        $this->conn = (new DBConnect())->getConnection(); // Tạo kết nối mới
-        $this->model = new ImportFileExcel($this->conn); // Truyền kết nối vào ImportFileExcel
+    public function __construct()
+    {
+        $this->conn = (new DBConnect())->getConnection();
+        $this->model = new ImportFileExcel($this->conn);
         $this->sqlQueries = new SQLQueries($this->conn);
     }
 
-    public function import() {
+    public function import()
+    {
+
+        ini_set("memory_limit", "512M");
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // Xử lý import dữ liệu từ file
+
+            if ($_FILES["fileToUpload"]["error"] != 0) {
+
+                echo "Lỗi: Có lỗi xảy ra khi tải tệp lên máy chủ!";
+                exit();
+            }
+
+            $maxFileSize = 1.9 * 1024 * 1024;
+            if ($_FILES["fileToUpload"]["size"] > $maxFileSize) {
+                echo "Lỗi: Kích thước tệp quá lớn. Vui lòng tải lên tệp nhỏ hơn 2MB!";
+                exit();
+            }
+
+            $fileExtension = strtolower(pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION));
+            if ($fileExtension != 'xlsx' && $fileExtension != 'xls') {
+                echo "Lỗi: Chỉ chấp nhận tệp Excel có định dạng .xlsx hoặc .xls!";
+                exit();
+            }
+
             $excelFile = $_FILES["fileToUpload"]["tmp_name"];
             $data = $this->model->importData($excelFile);
-            $this->addToDatabase($data);
-            echo "Import dữ liệu thành công!";
+            $result = $this->addToDatabase($data);
+
+            if ($result === false) return;
         } else {
-            // Hiển thị form import
-            require_once __DIR__. '/../view/import-excel.php';
-            echo "Import dữ liệu không thành công!";
+
+            require_once __DIR__ . '/../view/import-excel.php';
         }
     }
 
@@ -40,94 +66,207 @@ class ImportFileExcelController{
             ];
             $ma_hk = $this->insertHocKyAndGetMaHK($hoc_ky_data);
 
-            foreach ($data as $row) {
-                $this->insertMonHocIfNotExists($row);
-                $this->insertGiangVienIfNotExists($row);
-                $this->insertLopIfNotExists($row);
-                $this->insertLopMonHocIfNotExists($row, $ma_hk);
-            }
-            $this->conn->commit();
-            echo "Import dữ liệu thành công";
+            foreach (array_chunk($data, 100) as $chunk) {
+                foreach ($chunk as $row) {
+                    if (!$this->insertMonHocIfNotExists($row)) exit();
 
-        }catch (PDOException $e) {
+                    if (!$this->insertGiangVienIfNotExists($row)) exit();
+
+                    if (!$this->insertLopIfNotExists($row)) exit();
+
+                    if (!$this->updateOrInsertLopMonHoc($row, $ma_hk)) exit();
+                }
+            }
+
+            $this->conn->commit();
+            echo "success";
+        } catch (PDOException $e) {
             $this->conn->rollBack();
             echo "Lỗi: " . $e->getMessage();
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
         }
     }
 
     private function insertHocKyAndGetMaHK($hoc_ky_data)
     {
-        // Thêm mới một bản ghi vào bảng 'hoc_ky'
-        $ma_hk = $this->sqlQueries->insertData('hoc_ky', $hoc_ky_data);
-        // Trả về mã học kỳ mới được sinh tự động
-        return $ma_hk;
-    }
+        $result = $this->sqlQueries->selectData('hoc_ky', 'ma_hocky', 'ten_hocky = ? AND nam_hoc = ?', [$hoc_ky_data['ten_hocky'], $hoc_ky_data['nam_hoc']]);
 
-    private function insertMonHocIfNotExists($row) {
-        $result = $this->sqlQueries->selectData('mon_hoc','*','ma_monhoc = ?',[$row[4]]);
-
-        if (!$result) {
-
-            $data = [
-              'ma_monhoc' => $row[4],
-              'ten_monhoc' => $row[90]
-            ];
-            $this->sqlQueries->insertData('mon_hoc',$data);
+        // Nếu học kỳ đã tồn tại, trả về mã học kỳ
+        if (!empty($result)) {
+            return $result[0]['ma_hocky'];
         }
+
+        // Nếu không, thực hiện chèn mới và trả về mã học kỳ mới
+        return $this->sqlQueries->insertData('hoc_ky', $hoc_ky_data);
     }
 
-    private function insertGiangVienIfNotExists($row) {
-        $result = $this->sqlQueries->selectData('giang_vien', '*', 'ma_gv = ?', [$row[5]]);
-        if (!$result) {
-            $data = [
-                'ma_gv' => $row[5],
-                'ho_lot_gv' => $row[91],
-                'ten_gv' => $row[92]
-            ];
-            $this->sqlQueries->insertData('giang_vien', $data);
-        }
-    }
 
-    private function insertLopIfNotExists($row) {
-        $result = $this->sqlQueries->selectData('lop', '*', 'ma_lop = ?', [$row[82]]);
-
-        if (!$result) {
-            $data = [
-                'ma_lop' => $row[82],
-                'si_so' => $row[18]
-            ];
-            $this->sqlQueries->insertData('lop', $data);
-        }
-    }
-
-    private function insertLopMonHocIfNotExists($row, $ma_hk)
+    private function updateOrInsertLopMonHoc($row, $ma_hk)
     {
-        $result = $this->sqlQueries->selectData('lop_monhoc', '*', 'ma_lichday = ?',[$row[0]]);
-        if(!$result){
-            $data = [
-                'ma_lichday' => $row[0],
-                'thu' => $row[1],
-                'tiet_batdau' => $row[2],
-                'so_tiet' => $row[3],
-                'so_tietmonhoc' => $row[85],
-                'si_solop' => $row[81], //
-                'ma_lophoc' => $row[86],
-                'ten_phong' => $row[6],
-                'tiet_hoc' => $row[83],
-                'thoigian_hoc' => $row[76],
-                'ngay_batdau' => $row[89],
-                'ma_monhoc' => $row[4],
-                'ma_gv' => $row[5],
-                'ma_hk' => $ma_hk,//
-                'ma_lop' => $row[82],
+        if (isset($row[0]) && !empty($row[0])) {
+            if (empty($row[1]) || empty($row[2]) || empty($row[3]) || empty($row[85]) || empty($row[81]) || empty($row[86]) || empty($row[6]) || empty($row[83]) || empty($row[76]) || empty($row[89]) || empty($row[4]) || empty($row[5]) || empty($row[82])) {
+                echo "Lỗi: Dữ liệu lớp môn học không đầy đủ.";
+                return false;
+            }
 
-            ];
+            if (!is_numeric($row[0])) {
+                echo "Lỗi: 'Mã lớp môn học' phải là một chuỗi số nguyên.";
+                return false;
+            }
 
-            $this->sqlQueries->insertData('lop_monhoc',$data);
+            // Kiểm tra xem mã lớp môn học đã tồn tại hay không
+            $result = $this->sqlQueries->selectData('lop_monhoc', '*', 'ma_lopmonhoc = ? AND ma_hk = ?', [$row[0], $ma_hk]);
+
+            if ($result) {
+                $data = [
+                    'thu' => $row[1],
+                    'tiet_batdau' => $row[2],
+                    'so_tiet' => $row[3],
+                    'so_tietmonhoc' => $row[85],
+                    'si_solop' => $row[81],
+                    'ma_lophoc' => $row[86],
+                    'ten_phong' => $row[6],
+                    'tiet_hoc' => $row[83],
+                    'thoigian_hoc' => $row[76],
+                    'ngay_batdau' => $row[89],
+                    'ma_monhoc' => $row[4],
+                    'ma_gv' => $row[5],
+                    'ma_hk' => $ma_hk,
+                    'ma_lop' => $row[82]
+                ];
+
+                // Thực hiện UPDATE dữ liệu
+                $this->sqlQueries->updateData('lop_monhoc', $data, 'ma_lopmonhoc = ? AND ma_hk = ?', [$row[0], $ma_hk]);
+            } else {
+                // Nếu chưa tồn tại, thực hiện INSERT
+                $data = [
+                    'ma_lopmonhoc' => $row[0],
+                    'thu' => $row[1],
+                    'tiet_batdau' => $row[2],
+                    'so_tiet' => $row[3],
+                    'so_tietmonhoc' => $row[85],
+                    'si_solop' => $row[81],
+                    'ma_lophoc' => $row[86],
+                    'ten_phong' => $row[6],
+                    'tiet_hoc' => $row[83],
+                    'thoigian_hoc' => $row[76],
+                    'ngay_batdau' => $row[89],
+                    'ma_monhoc' => $row[4],
+                    'ma_gv' => $row[5],
+                    'ma_hk' => $ma_hk,
+                    'ma_lop' => $row[82]
+                ];
+
+                $this->sqlQueries->insertData('lop_monhoc', $data);
+
+                print($data);
+            }
+            return true;
+        } else {
+            echo "Lỗi: 'Mã lớp môn học' không được gửi hoặc bị thiếu.";
+            return false;
         }
-
     }
 
+
+    private function insertMonHocIfNotExists($row)
+    {
+
+        if (isset($row[4]) && !empty($row[4])) {
+
+            if (empty($row[90])) {
+                echo "Lỗi: Dữ liệu môn học không đầy đủ.";
+                return false;
+            }
+
+            if (!is_string($row[4])) {
+                echo "Lỗi: 'Mã môn học' phải là một chuỗi.";
+                return false;
+            }
+
+            $result = $this->sqlQueries->selectData('mon_hoc', '*', 'ma_monhoc = ?', [$row[4]]);
+            if (!$result) {
+                $data = [
+                    'ma_monhoc' => $row[4],
+                    'ten_monhoc' => $row[90]
+                ];
+                $this->sqlQueries->insertData('mon_hoc', $data);
+                return true;
+            } else {
+
+                return true;
+            }
+        } else {
+            echo "Lỗi: 'mã môn học' không được gửi hoặc bị thiếu.";
+            return false;
+        }
+    }
+
+
+    private function insertGiangVienIfNotExists($row)
+    {
+        if (isset($row[5]) && !empty($row[5])) {
+            if (empty($row[91]) || empty($row[92])) {
+                echo "Lỗi: Dữ liệu giảng viên không đầy đủ.";
+                return false;
+            }
+
+            if (!is_string($row[5])) {
+                echo "Lỗi: 'Mã giảng viên' phải là một chuỗi.";
+                return false;
+            }
+
+            $result = $this->sqlQueries->selectData('giang_vien', '*', 'ma_gv = ?', [$row[5]]);
+            if (!$result) {
+                $data = [
+                    'ma_gv' => $row[5],
+                    'ho_lot_gv' => $row[91],
+                    'ten_gv' => $row[92]
+                ];
+                $this->sqlQueries->insertData('giang_vien', $data);
+                return true;
+            } else {
+
+                return true;
+            }
+        } else {
+            echo "Lỗi: 'Mã giảng viên' không được gửi hoặc bị thiếu.";
+            return false;
+        }
+    }
+
+    private function insertLopIfNotExists($row)
+    {
+
+        if (isset($row[82]) && !empty($row[82])) {
+            if (empty($row[18])) {
+                echo "Lỗi: Dữ liệu lớp không đầy đủ.";
+                return false;
+            }
+
+            if (!is_string($row[82])) {
+                echo "Lỗi: 'Mã lớp' phải là một chuỗi.";
+                return false;
+            }
+
+            $result = $this->sqlQueries->selectData('lop', '*', 'ma_lop = ?', [$row[82]]);
+            if (!$result) {
+                $data = [
+                    'ma_lop' => $row[82],
+                    'si_so' => $row[18]
+                ];
+                $this->sqlQueries->insertData('lop', $data);
+                return true;
+            } else {
+
+                return true;
+            }
+        } else {
+            echo "Lỗi: 'Mã lớp' không được gửi hoặc bị thiếu.";
+            return false;
+        }
+    }
 }
 
 $importExcel = new ImportFileExcelController();
@@ -137,5 +276,3 @@ switch ($action) {
         $importExcel->import();
         break;
 }
-
-
--- controller/UserController.php
+++ controller/UserController.php
@@ -1,27 +1,18 @@
 <?php
-require_once "../DB/DBConnect.php";
-require_once "../model/User.php";
-
 
 require_once __DIR__ . '/../Helper/ConfigHelper.php';
-require_once  HELPER_PATH . 'ResponseHelper.php';
+
+require_once MODEL_PATH . 'User.php';
+require_once DB_PATH . 'DBConnect.php';
+
 session_start();
 class UserController
 {
-  private $model;
-  private $conn;
-  private $responseHelper;
-
-  public function __construct()
-  {
-    $this->conn = (new DBConnect())->getConnection();
-    $this->model = new User($this->conn);
-    $this->responseHelper = new ResponseHelper();
-  }
   public function login()
   {
     $this->checkLoginInfo();
-    $this->getUser();
+    $user = $this->getUser();
+    $this->roleUser($user);
   }
 
   //Kiem tra TK va MK
@@ -30,17 +21,17 @@ class UserController
     $username = isset($_POST["username"]) ? $_POST["username"] : "";
     $password = isset($_POST["password"]) ? $_POST["password"] : "";
     if ($username == "") {
-      $_SESSION["thongbao"] = "Tài khoản hoặc mật khẩu không đúng";
+      $_SESSION["thongbao"] = "Tài khoản và mật khẩu không đúng";
       header("Location: ../index.php");
       exit();
     }
     if ($password == "") {
-      $_SESSION["thongbao"] = "Tài khoản hoặc mật khẩu không đúng";
+      $_SESSION["thongbao"] = "Tài khoản và mật khẩu không đúng";
       header("Location: ../index.php");
       exit();
     }
-    if (strlen($username) < 10  && strlen($username) > 15) {
-      $_SESSION["thongbao"] = "Tài khoản hoặc mật khẩu không đúng";
+    if (strlen($username) <= 10  || strlen($username) >= 15) {
+      $_SESSION["thongbao"] = "Tài khoản và mật khẩu không đúng";
       header("Location: ../index.php");
       exit();
     }
@@ -48,12 +39,12 @@ class UserController
       if (preg_match('/[A-Z]/', $password) && preg_match('/[^\w]/', $password)) {
         return true;
       } else {
-        $_SESSION["thongbao"] = "Tài khoản hoặc mật khẩu không đúng";
+        $_SESSION["thongbao"] = "Tài khoản và mật khẩu không đúng";
         header("Location: ../index.php");
         exit();
       }
     } else {
-      $_SESSION["thongbao"] = "Tài khoản hoặc mật khẩu không đúng";
+      $_SESSION["thongbao"] = "Tài khoản và mật khẩu không đúng";
       header("Location: ../index.php");
       exit();
     }
@@ -64,44 +55,35 @@ class UserController
   {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $password = md5($password);
-    $user = $this->model->selectUserID('tai_khoan', '*', 'username = ? AND password = ?', [$username, $password]);
-    if ($user) {
-      if ($user['is_active'] == 0) {
-        $_SESSION["thongbao"] = "Tài khoản đã bị khóa";
-        header("Location: ../view/login.php");
-        exit();
-      }
+    $user = new User((new DBConnect())->getConnection());
+    return $user->getUser($username, $password);
+  }
 
-      if ($user['role'] == 'admin') {
-        $_SESSION['id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['role'] = $user['role'];
-        $_SESSION['login'] = true;
-        header("Location: ../view/admin/layout-admin.php");
-        exit();
-      } elseif ($user['role'] == 'user' && $user['ma_gv'] !== null) {
-        $_SESSION['id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['role'] = $user['role'];
-        $_SESSION['ma_gv'] = $user['ma_gv'];
-        $_SESSION['login'] = true;
-        header("Location: ../view/thong-ke-canhan.php");
-        exit();
-      } else {
-        $_SESSION['thongbao'] = 'Tài khoản hoặc mật khẩu không đúng';
-        header("Location: ../view/login.php");
-        exit();
-      }
+  //Chuyen trang theo role
+  private function roleUser($user)
+  {
+    if ($user && $user['role'] == 'admin') {
+      $_SESSION['id'] = $user['id'];
+      $_SESSION['username'] = $user['username'];
+      $_SESSION['role'] = $user['role'];
+      $_SESSION['login'] = true;
+      header("Location: ../view/admin/layout-admin.php");
+      exit();
+    } elseif ($user && $user['role'] == 'user') {
+      $_SESSION['id'] = $user['id'];
+      $_SESSION['username'] = $user['username'];
+      $_SESSION['role'] = $user['role'];
+      $_SESSION['login'] = true;
+      header("Location: ../view/user/layout-user.php");
+      exit();
     } else {
       $_SESSION['thongbao'] = 'Tài khoản hoặc mật khẩu không đúng';
       header("Location: ../view/login.php");
-      exit();
     }
   }
   public function changePassword()
   {
-    $id = $_POST['id'];
+    $username = $_POST['username'];
     $newPassword = $_POST['newPassword'];
     $oldPassword = $_POST['oldPassword'];
     $re_new_password = $_POST['re_new_password'];
@@ -123,278 +105,20 @@ class UserController
     }
     if (isset($message)) {
       $_SESSION["thongbao"] = $message;
-      header("Location: ../view/change-password.php");
-
+      header("Location: /view/admin/change-password.php");
       exit();
     } else {
-      $table = 'tai_khoan';
-      $data = [
-        'password' => md5($newPassword),
-      ];
-      $condition = 'id = ?';
-      $result = $this->model->update($table, $data, $condition, $id);
+      $user = new User((new DBConnect())->getConnection());
+      $result = $user->changePasswordModel($username, $newPassword, $oldPassword);
       if ($result) {
         header("Location: ../../index.php");
         exit();
       } else {
         $_SESSION["thongbao"] = "Mật khẩu cũ không đúng";
-        header("Location: ../view/change-password.php");
+        header("Location: /view/admin/change-password.php");
         exit();
       }
     }
-  }
-
-  public function changePasswordUser()
-  {
-    $id = $_POST['id'];
-    $newPassword = $_POST['newPassword'];
-    $oldPassword = $_POST['oldPassword'];
-    $re_new_password = $_POST['re_new_password'];
-
-    if ($newPassword == "" || $oldPassword == "" || $re_new_password == "") {
-      $message = "Vui lòng nhập đầy đủ thông tin";
-    } elseif (strlen($oldPassword) < 10 || strlen($oldPassword) > 30) {
-      $message = "Mật khẩu cũ không đúng";
-    } elseif (strlen($newPassword) < 10 || strlen($newPassword) > 30) {
-      $message = "Mật khẩu mới không đúng";
-    } elseif (strlen($re_new_password) < 10 || strlen($re_new_password) > 30) {
-      $message = "Mật khẩu nhập lại không đúng";
-    } elseif (!preg_match('/[A-Z]/', $newPassword) || !preg_match('/[^\w]/', $newPassword)) {
-      $message = "Mật khẩu mới phải có ít nhất một chữ hoa và một ký tự đặc biệt";
-    } elseif ($newPassword == $oldPassword) {
-      $message = "Mật khẩu mới không được trùng với mật khẩu cũ";
-    } elseif ($newPassword != $re_new_password) {
-      $message = "Mật khẩu nhập lại không đúng";
-    }
-    if (isset($message)) {
-      $_SESSION["thongbao"] = $message;
-      header("Location: ../view/change-password-user.php");
-
-      exit();
-    } else {
-      $table = 'tai_khoan';
-      $data = [
-        'password' => md5($newPassword),
-      ];
-      $condition = 'id = ?';
-      $result = $this->model->update($table, $data, $condition, $id);
-      if ($result) {
-        header("Location: ../../index.php");
-        exit();
-      } else {
-        $_SESSION["thongbao"] = "Mật khẩu cũ không đúng";
-        header("Location: ../view/change-password-user.php");
-        exit();
-      }
-    }
-  }
-
-
-
-
-
-  public function delete()
-  {
-    $id = $_GET['id'];
-    $table = 'tai_khoan';
-    $condition = 'id = ?';
-    $result = $this->model->deleteUser($table, $condition, [$id]);
-    if ($result) {
-      header("Location: /view/list_taikhoan.php");
-      exit();
-    } else {
-      $_SESSION["thongbao"] = "Xóa không thành công";
-      header("Location: /view/admin/list_taikhoan.php");
-      exit();
-    }
-  }
-  public function insert()
-  {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $username = $this->xoaKhoangTrang($username);
-    $password = $this->xoaKhoangTrang($password);
-    $this->kiemTraTaiKhoanTrung("../view/them_taikhoan.php");
-    $role = $_POST['role'];
-    $status = $_POST['status'];
-    $ma_gv = ($_POST['giangvien'] !== "") ? $_POST['giangvien'] : null;
-
-    $data = [
-      'username' => $username,
-      'password' => md5($password),
-      'role' => $role,
-      'is_active' => $status,
-      'ma_gv' => $ma_gv
-    ];
-    $table = 'tai_khoan';
-    $result = $this->model->insertUser($table, $data);
-    if ($result) {
-      header("Location: /view/list_taikhoan.php");
-      exit();
-    } else {
-      header("Location: /view/them_taikhoan.php");
-      exit();
-    }
-  }
-  public function checkInsert()
-  {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    if (isset($_POST['submit'])) {
-      if (empty($_POST['username'])) {
-        $_SESSION["error_username"] = "Vui lòng nhập tài khoản";
-        header("Location: ../view/them_taikhoan.php");
-        exit();
-      }
-      if (strlen($username) < 10 || strlen($username) > 15) {
-        $_SESSION["error_username"] = "Tài khoản phải có từ 10 đến 15 ký tự";
-        header("Location: ../view/them_taikhoan.php");
-        exit();
-      }
-
-      if (empty($_POST['password'])) {
-        $_SESSION["error_password"] = "Vui lòng nhập mật khẩu";
-        header("Location: ../view/them_taikhoan.php");
-        exit();
-      }
-      if (strlen($password) < 10 || strlen($password) > 15) {
-        $_SESSION["error_password"] = "Mật khẩu phải có từ 10 đến 15 ký tự";
-        header("Location: ../view/them_taikhoan.php");
-        exit();
-      }
-
-      if (!preg_match('/[A-Z]/', $password) || !preg_match('/[^\w]/', $password)) {
-        $_SESSION["error_password"] = "Mật khẩu phải chứa ít nhất một chữ hoa và một ký tự đặc biệt";
-        header("Location: ../view/them_taikhoan.php");
-        exit();
-      }
-    }
-  }
-  public function update()
-  {
-    $id = $_POST['id'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $username = $this->xoaKhoangTrang($username);
-    $password = $this->xoaKhoangTrang($password);
-    $table = 'tai_khoan';
-    $condition = 'id = ?';
-    $this->kiemTraTaiKhoanTrung("../view/sua_taikhoan.php?id=" . $id);
-    $user_check = $this->model->selectUserID($table, '*', $condition, [$id]);
-    if ($user_check['password'] == $password) {
-      $role = $_POST['role'];
-      $status = $_POST['status'];
-      $ma_gv = isset($_POST['giangvien_disabled']) ? $_POST['giangvien_disabled'] : null;
-      if (empty($ma_gv)) {
-        $ma_gv = null;
-      }
-
-      $table = 'tai_khoan';
-      $condition = 'id = ?';
-      $password = $password;
-      $data = [
-        'username' => $username,
-        'password' => $password,
-        'role' => $role,
-        'is_active' => $status,
-        'ma_gv' => $ma_gv
-      ];
-      $result = $this->model->update($table, $data, $condition, $id);
-      if ($result) {
-        header("Location: /view/list_taikhoan.php");
-        exit();
-      } else {
-        header("Location: /view/list_taikhoan.php");
-        exit();
-      }
-    } else {
-      if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
-        if (empty($_POST['password'])) {
-          $_SESSION["error_password"] = "Vui lòng nhập mật khẩu";
-          header("Location: ../view/sua_taikhoan.php?id=" . $id);
-          exit();
-        }
-        if (strlen($password) < 10 || strlen($password) > 15) {
-          $_SESSION["error_password"] = "Mật khẩu phải có từ 10 đến 15 ký tự";
-          header("Location: ../view/sua_taikhoan.php?id=" . $id);
-          exit();
-        }
-        if (!preg_match('/[A-Z]/', $password) || !preg_match('/[^\w]/', $password)) {
-          $_SESSION["error_password"] = "Mật khẩu phải chứa ít nhất một chữ hoa và một ký tự đặc biệt";
-          header("Location: ../view/sua_taikhoan.php?id=" . $id);
-          exit();
-        }
-      }
-      $role = $_POST['role'];
-      $status = $_POST['status'];
-
-      $ma_gv = isset($_POST['giangvien_disabled']) ? $_POST['giangvien_disabled'] : null;
-      if (empty($ma_gv)) {
-        $ma_gv = null;
-      }
-
-      $table = 'tai_khoan';
-      $condition = 'id = ?';
-      $password = md5($password);
-      $data = [
-        'username' => $username,
-        'password' => $password,
-        'role' => $role,
-        'is_active' => $status,
-        'ma_gv' => $ma_gv
-      ];
-      $result = $this->model->update($table, $data, $condition, $id);
-      if ($result) {
-        header("Location: /view/list_taikhoan.php");
-        exit();
-      } else {
-        header("Location: /view/list_taikhoan.php");
-        exit();
-      }
-    }
-  }
-  public function checkUpdate()
-  {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $id = $_POST['id'];
-    if (isset($_POST['submit'])) {
-      if (empty($_POST['username'])) {
-        $_SESSION["error_username"] = "Vui lòng nhập tài khoản";
-        header("Location: ../view/list_taikhoan.php?id=" . $id);
-        exit();
-      }
-      if (strlen($username) < 10 || strlen($username) > 15) {
-        $_SESSION["error_username"] = "Tài khoản phải có từ 10 đến 15 ký tự";
-        header("Location: ../view/list_taikhoan.php?id=" . $id);
-        exit();
-      }
-    }
-  }
-  public function kiemTraTaiKhoanTrung($redirectUrl)
-  {
-    $id = isset($_POST['id']) ? $_POST['id'] : '';
-    $username = $_POST['username'];
-    $username = $this->xoaKhoangTrang($username);
-    $checkUser = $this->model->selectUserID('tai_khoan', '*', 'username = ? AND id != ?', [$username, $id]);
-    if ($checkUser) {
-      $_SESSION["error_username"] = " Tài khoản đã được đăng ký";
-      header("Location: " . $redirectUrl);
-      exit();
-    }
-  }
-  public function xoaKhoangTrang($str)
-  {
-    $str = trim($str);
-    $str = preg_replace('/\s+/', '', trim($str));
-    return $str;
-  }
-
-  public function getAccountbyMagv()
-  {
-    $ma_gv = $_SESSION['ma_gv'] ?? null;
-    $data = $this->model->getAccountByMagv($ma_gv);
-    return $this->responseHelper->Response(true, "Lấy dữ liệu thành công!", $data);
   }
 }
 $userController = new UserController();
@@ -405,24 +129,5 @@ switch ($action) {
     break;
   case 'changePassword':
     $userController->changePassword();
-    break;
-  case 'changePasswordUser':
-    $userController->changePasswordUser();
-    break;
-  case 'delete':
-    $userController->delete();
-    break;
-  case 'insert':
-    $userController->checkInsert();
-    $userController->insert();
-    break;
-  case 'update':
-    $userController->checkUpdate();
-    $userController->update();
-  case 'getAccountbyMagv':
-    $userController->getAccountbyMagv();
-    break;
-  default:
-    echo "Action không tồn tại!";
     break;
 }
